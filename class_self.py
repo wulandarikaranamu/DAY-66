acbfd47c09e872348f293890440781f18ce0ebb6
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-15 22:02:12 +0700

    komentar

---
+++ class_self.py
@@ -0,0 +1,12 @@
+#belajar class pada python
+
+class pelajar :
+    def __init__(self,nama,umur,alamat):
+        self.nama = nama
+        self.umur = umur
+        self.alamat = alamat
+a = pelajar("merry","18","Tapalang")
+
+print(a.nama)
+print(a.umur)
+print(a.alamat)
